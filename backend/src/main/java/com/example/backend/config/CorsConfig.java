b980dcea9d4ac3d8f4db1891c515b9e0f73f9251
Mayra Denot <mayra.denot@gmail.com>
2026-05-31 17:14:23 -0300

    Agregué la url del deploy en configuracion de cors

--- backend/src/main/java/com/example/backend/config/CorsConfig.java
+++ backend/src/main/java/com/example/backend/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://proximamente"
+                "https://ecommerce-app-ten-mocha.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
